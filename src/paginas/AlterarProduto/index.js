ea84ceea1a50bc4f9ddafbdae1ca5df68f599788
Dev_Matheus <68796271+TheuusWmv@users.noreply.github.com>
2026-05-16 11:26:43 -0300

    Fix: Correção das dependencias da stack de Navegação

--- src/paginas/AlterarProduto/index.js
+++ src/paginas/AlterarProduto/index.js
@@ -1,11 +1,12 @@
-import React, {useState, useEffect, useCallback} from "react";
-import { View, Text, TouchableOpacity, FlatList, RefreshControl, SafeAreaViewBase } from "react-native";
+import React from "react";
+import { View, Text } from "react-native";
 
-import api from "../../servicos/api";
 import style from "./style";
 
-import FontAwesome from "@expo/vector-icons";
-
 export default function AlterarProduto({}){
-    
+    return (
+        <View style={style.container}>
+            <Text>Alterar produto</Text>
+        </View>
+    );
 }
